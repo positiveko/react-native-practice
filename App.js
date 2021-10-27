0e75a166ded8f0ab8f951f065d549602657e720e
positiveko <edie.spark1er@gmail.com>
2021-10-27 14:06:17 +0900

    feat: loadFonts, loadImages fuc 추가

--- App.js
+++ App.js
@@ -2,19 +2,29 @@ import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { Text, Image } from 'react-native';
 import { Asset } from 'expo-asset';
-
 import * as Font from 'expo-font';
-
 import { Ionicons } from '@expo/vector-icons';
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+    return Asset.loadAsync(image);
+  });
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require('./positiveko.jpeg'));
-    // prefetch보다는 loadAsync로 로컬 파일 require하는 방식 사용할 것
-    await Image.prefetch('https://reactnative.dev/img/oss_logo.png');
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require('./positiveko.jpeg'),
+      'https://reactnative.dev/img/oss_logo.png',
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
 
   if (!ready) {
