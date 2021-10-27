d69a8262aa991f24df1953052ff2ee2d7b1729f1
positiveko <edie.spark1er@gmail.com>
2021-10-27 15:27:01 +0900

    design: navigation configuration 수정

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Movies from "../screens/Movies";
-import Tv from "../screens/Tv";
-import Search from "../screens/Search";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Movies from '../screens/Movies';
+import Tv from '../screens/Tv';
+import Search from '../screens/Search';
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
+  <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: { backgroundColor: 'tomato' },
+      tabBarActiveTintColor: 'yellow',
+      tabBarInactiveTintColor: 'white',
+      headerTitleStyle: { color: 'tomato' },
+      headerRight: () => (
+        <View>
+          <Text>⛳️</Text>
+        </View>
+      ),
+    }}>
+    <Tab.Screen name='Movies' component={Movies} />
+    <Tab.Screen name='Tv' component={Tv} />
+    <Tab.Screen name='Search' component={Search} />
   </Tab.Navigator>
 );
 
