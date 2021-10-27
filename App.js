17a451f13de36d7c974f7bb33f17ecb612a3a99c
positiveko <edie.spark1er@gmail.com>
2021-10-27 14:11:18 +0900

    refactor: useAssets, useFonts 리팩토링

--- App.js
+++ App.js
@@ -1,40 +1,16 @@
 import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { Text, Image } from 'react-native';
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    }
-    return Asset.loadAsync(image);
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require('./positiveko.jpeg'),
-      'https://reactnative.dev/img/oss_logo.png',
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
+  const [assets] = useAssets([require('./positiveko.jpeg')]);
+  const [loaded] = Font.useFonts(Ionicons.font);
 
-  if (!ready) {
-    return (
-      <AppLoading
-        onFinish={onFinish}
-        onError={console.error}
-        startAsync={startLoading}
-      />
-    );
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done</Text>;
 }
