03f76e0467a439f518f2228eed94e8c390251491
positiveko <edie.spark1er@gmail.com>
2021-10-27 15:33:29 +0900

    design: darkmode color config 설정

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, useColorScheme } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
+import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from '../colors';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: { backgroundColor: 'tomato' },
-      tabBarActiveTintColor: 'yellow',
-      tabBarInactiveTintColor: 'white',
-      headerTitleStyle: { color: 'tomato' },
-      headerRight: () => (
-        <View>
-          <Text>⛳️</Text>
-        </View>
-      ),
-    }}>
-    <Tab.Screen name='Movies' component={Movies} />
-    <Tab.Screen name='Tv' component={Tv} />
-    <Tab.Screen name='Search' component={Search} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  // shift + command + a : 다크모드 변경
+  const isDark = useColorScheme() === 'dark';
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : 'white',
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : 'white',
+        },
+        headerTitleStyle: {
+          color: isDark ? 'white' : BLACK_COLOR,
+        },
+      }}>
+      <Tab.Screen name='Movies' component={Movies} />
+      <Tab.Screen name='Tv' component={Tv} />
+      <Tab.Screen name='Search' component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
