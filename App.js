d1b269d86187c89ba82c31e05429d69762290848
positiveko <edie.spark1er@gmail.com>
2021-10-27 16:53:44 +0900

    feat: styled components 및 theme provider 추가

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { useAssets } from 'expo-asset';
+import React, { useState } from 'react';
 import * as Font from 'expo-font';
+import { useColorScheme } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import Root from './navigation/Root';
+import { ThemeProvider } from 'styled-components/native';
+import { darkTheme, lightTheme } from './styled';
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-  const [assets] = useAssets([require('./positiveko.jpeg')]);
-  const [loaded] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
 
-  if (!assets || !loaded) {
-    return <AppLoading />;
+  const isDark = useColorScheme() === 'dark';
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import styled from 'styled-components/native';
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate('Stack', { screen: 'Three' })}
-    style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate('Stack', { screen: 'Three' })}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
 );
+
 export default Movies;
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.mainBgColor};
+`;
+
+const Title = styled.Text`
+  color: ${({ theme }) => theme.textColor};
+`;
