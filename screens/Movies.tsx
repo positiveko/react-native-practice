56f6f1b7ad906330b12d33686782b92a6c5407e1
positiveko <edie.spark1er@gmail.com>
2021-10-28 18:13:01 +0900

    feat: upcoming moview list 추가

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -5,6 +5,7 @@ import Swiper from 'react-native-swiper';
 import styled from 'styled-components/native';
 import { API_KEY } from 'react-native-dotenv';
 import Slide from '../components/Slide';
+import Poster from '../components/Poster';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
@@ -26,7 +27,7 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
   const getUpcoming = async () => {
     const { results } = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
       )
     ).json();
     setUpcoming(results);
@@ -63,7 +64,11 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         autoplayTimeout={3.5}
         showsButtons={false}
         showsPagination={false}
-        containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}>
+        containerStyle={{
+          marginBottom: 30,
+          width: '100%',
+          height: SCREEN_HEIGHT / 4,
+        }}>
         {nowPlaying.map((movie) => (
           <Slide
             key={movie.id}
@@ -75,6 +80,22 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
           />
         ))}
       </Swiper>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}>
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? '...' : null}
+            </Title>
+            <Votes>⭐️ {movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
@@ -89,4 +110,32 @@ const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+
+const ListTitle = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+
+const Votes = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
 `;
