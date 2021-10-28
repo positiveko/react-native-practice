8d24402d79d7c635b50f4546317bd908e07e9de0
positiveko <edie.spark1er@gmail.com>
2021-10-28 18:05:13 +0900

    refactor: slider, poster 컴포넌트 분리

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,22 +1,37 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useEffect, useState } from 'react';
-import {
-  ActivityIndicator,
-  Dimensions,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
+import { ActivityIndicator, Dimensions } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Swiper from 'react-native-swiper';
 import styled from 'styled-components/native';
-import { BlurView } from 'expo-blur';
-import { makeImgPath } from '../utils';
 import { API_KEY } from 'react-native-dotenv';
+import Slide from '../components/Slide';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
-  const isDark = useColorScheme() === 'dark';
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+      )
+    ).json();
+    setTrending(results);
+  };
+
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+      )
+    ).json();
+    setUpcoming(results);
+  };
+
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -24,11 +39,17 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
       )
     ).json();
     setNowPlaying(results);
+  };
+
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
     setLoading(false);
   };
+
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
+
   return loading ? (
     <Loader>
       <ActivityIndicator />
@@ -44,29 +65,14 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         showsPagination={false}
         containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}>
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            />
-            <BlurView
-              tint={isDark ? 'dark' : 'light'}
-              intensity={85}
-              style={StyleSheet.absoluteFill}>
-              <Wrapper>
-                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-                <Column>
-                  <Title isDark={isDark}>{movie.original_title}</Title>
-                  {movie.vote_average > 0 ? (
-                    <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
-                  ) : null}
-                  <Overview isDark={isDark}>
-                    {movie.overview.slice(0, 100)}...
-                  </Overview>
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
     </Container>
@@ -79,44 +85,8 @@ const Container = styled.ScrollView`
   background-color: ${({ theme }) => theme.mainBgColor};
 `;
 
-const View = styled.View`
-  flex: 1;
-`;
-
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`;
-
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-const Title = styled.Text<{ isDark: boolean }>`
-  font-size: 16px;
-  font-weight: 600;
-  color: ${({ isDark, theme }) => (isDark ? 'white' : theme.textColor)};
-`;
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  width: 90%;
-  margin: 0 auto;
-  justify-content: space-around;
-  align-items: center;
-`;
-const Column = styled.View`
-  width: 60%;
-`;
-const Overview = styled.Text<{ isDark: boolean }>`
-  margin-top: 10px;
-  color: ${({ isDark }) =>
-    isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
-`;
-const Votes = styled(Overview)`
-  font-size: 12px;
 `;
